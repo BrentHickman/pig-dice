176d1bc6b28afd0f3905284255d3c51832b4b432
Brent <BrentHickman@ymail.com>
2023-01-25 14:40:13 -0800

    WIP setting up hold button to store current score in total score
    Co-authored-by: Viradeth <xayananh4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,95 @@
 let total = 0;
-
+let scoresDB = new ScoresDB();
 //Business Logic
-
-function Player(currentRoll, totalScore){
-  this.currentRoll = currentRoll;
-  this.totalScore = totalScore;
+function ScoresDB(){
+  this.players = {};
+  this.currentId = 0;
 }
+
+ScoresDB.prototype.addPlayer = function(player) {
+  player.id = this.assignId();
+  this.players[player.id] = player;
+};
+
+ScoresDB.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+ScoresDB.prototype.findPlayer = function(id) {
+  if (this.players[id] !== undefined) {
+    return this.players[id];
+  }
+  return false;
+};
+
+Player.prototype.addTotal = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+function Player(currentRoll, currentScore, totalScore){
+  
+  this.currentRoll = currentRoll;
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
+};
 
 function rollDice(){
   const rndInt = Math.floor(Math.random() *6) +1;
   return rndInt;
-}
+};
 
-function generateTotalScore(result){
-  total = result + total;
+function generateTotalScore(currentRoll){
+  total = currentRoll + total;
   return total;
-}
+};
 
 // UI Logic
 
-function handleHold(){
-  document.getElementById("play1").disabled = true;
-  document.getElementById("hold1").disabled = true;
-  document.getElementById("play2").disabled = false;
-  document.getElementById("hold2").disabled = false;
+
+function handleHold(currentScore){
+  const player1 = scoresDB.findPlayer(1);
+  let totalScore1 = player1.currentScore;
+    document.getElementById("play1").disabled = true;
+    document.getElementById("hold1").disabled = true;
+    window.alert("current score:" + totalScore1);
 }
 
 function handlePlaySubmission(event){
   event.preventDefault();
-
-
+  let totalScore = total;
   const currentRoll = rollDice();
-  let totalScore = "Current Score: " + generateTotalScore(currentRoll);
-  let player1 = new Player(currentRoll, totalScore);
-  
+  let currentScore = generateTotalScore(currentRoll);
+  let player1 = new Player(currentRoll, currentScore, totalScore);
+  handleHold(currentScore);
+  scoresDB.addPlayer(player1);
+
+
+  if(currentRoll <= 1)
+  {
+  player1.totalScore = 0;
   document.querySelector("div#rollP1").innerText = " Current Roll: " + player1.currentRoll;
   document.querySelector("div#scoreP1").innerText = player1.totalScore;
+  document.getElementById("play1").disabled = true;
+  document.getElementById("hold1").disabled = true
+}
+  
+  else {
+    document.querySelector("div#rollP1").innerText = "Current Roll: " + player1.currentRoll;
+    document.querySelector("div#scoreP1").innerText = "Current Score: " + player1.currentScore;
+  }
 
 }
 
 window.addEventListener("load", function() {
   let holdButton = document.querySelector("button#hold1");
   document.querySelector("form#game1").addEventListener("submit", handlePlaySubmission);
+  
+  
+  
   holdButton.addEventListener("click", handleHold);
-  document.querySelector("form#game2").addEventListener("submit", handlePlaySubmission);
+  // document.querySelector("form#game2").addEventListener("submit", handlePlaySubmission);
 
 })
 
